89ebe0094ac44676b120c5e6f17c68d478fad072
Sudhir Jonathan <sudhir.j@gmail.com>
2013-06-02 10:33:38 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lulu::Application.config.secret_key_base = '1683d8e3ca06b552af00354b9d47b2aa867f85aff37de1472fb28d3dd402a06b72041f975523906082e9c3a1fe688879b95ee1f91b357a984a529918d3c93eda'
